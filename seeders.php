3267e1b6d47e3390d6d1cb1c8eb507cab0dee772
Luigi213 <reyesgianluigi88@gmail.com>
2023-10-02 17:30:00 +0200

    optimized first istruction

--- seeders.php
+++ seeders.php
@@ -1,49 +1,43 @@
 <?php
 // Array di coppie email e password da inserire
-$datiUtenti = array(
-    array('email1@example.com', 'password1'),
-    array('email2@example.com', 'password2'),
-    array('email3@example.com', 'password3')
-);
-
-// Connessione al database (sostituisci con i tuoi dati di connessione)
-$servername = "localhost";
-$username = "root";
-$password = "root";
-$dbname = "db-edusogno";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connessione al database fallita: " . $conn->connect_error);
-}
-
-// Query SQL preparata per l'inserimento dei dati
-$sql = "INSERT INTO utente (email, password) VALUES (?, ?)";
-
-// Preparazione della dichiarazione SQL
-$stmt = $conn->prepare($sql);
-
-if ($stmt) {
-    // Ciclo per inserire tutte le coppie email e password
-    foreach ($datiUtenti as $dati) {
-        $email = $dati[0];
-        $password = $dati[1];
-
-        // Collegamento dei parametri e binding dei valori
-        $stmt->bind_param("ss", $email, $password);
-
-        // Esecuzione della query
-        $stmt->execute();
+class UtenteS {
+    public function upS($conn){
+        
+        $datiUtenti = array(
+            array('email1','example1','email1@example.com', 'password1'),
+            array('email2','example2','email2@example.com', 'password2'),
+            array('email3','example3','email3@example.com', 'password3')
+        );
+        
+        // Query SQL preparata per l'inserimento dei dati
+        $sql = "INSERT INTO utente (nome, cognome, email, password) VALUES (?, ?, ?, ?)";
+        
+        // Preparazione della dichiarazione SQL
+        $stmt = $conn->prepare($sql);
+        
+        if ($stmt) {
+            // Ciclo per inserire tutte le coppie email e password
+            foreach ($datiUtenti as $dati) {
+                $nome = $dati[0];
+                $cognome = $dati[1];
+                $email = $dati[2];
+                $password = $dati[3];
+        
+                // Collegamento dei parametri e binding dei valori
+                $stmt->bind_param("ssss", $nome, $cognome, $email, $password);
+        
+                // Esecuzione della query
+                $stmt->execute();
+            }
+        
+            // Chiusura della dichiarazione SQL preparata
+            $stmt->close();
+        } else {
+            echo "Errore nella preparazione della query: " . $conn->error;
+        }
+        
+        return true;
     }
-
-    // Chiusura della dichiarazione SQL preparata
-    $stmt->close();
-} else {
-    echo "Errore nella preparazione della query: " . $conn->error;
 }
-
-// Chiusura della connessione al database
-$conn->close();
 
 ?>
--- server.php
+++ server.php
@@ -1,38 +1,57 @@
-<?php
+    <?php
 
-    $servername = "localhost";
-    $username = "root";
-    $password = "root";
-    $dbname = "db-edusogno";
+        $servername = "localhost";
+        $username = "root";
+        $password = "root";
+        $dbname = "db-edusogno";
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-    if ($conn->connect_error) {
-        die("Connessione al database fallita: " . $conn->connect_error);
-    }
-
-    // Includi tutte le tue classi di migrazione
-    include_once("migrate.php");
-    // Aggiungi altre classi di migrazione secondo necessità
-
-    // Lista delle classi di migrazione
-    $migrazioni = [
-        new Utente(),
-    ];
-
-    foreach ($migrazioni as $migrazione) {
-        
-        if ($migrazione) {
-            if ($migrazione->up($conn)) {
-                
-                echo "Migrazione completata con successo: " . get_class($migrazione) . "\n";
-            } else {
-                echo "Errore nella migrazione: " . $conn->error . "\n";
-            }
-        } else {
-            echo "La migrazione è già stata eseguita: " . get_class($migrazione) . "\n";
+        if ($conn->connect_error) {
+            die("Connessione al database fallita: " . $conn->connect_error);
         }
-    }
 
-    $conn->close();
-?>
+        // Includi tutte le tue classi di migrazione
+        include_once("migrate.php");
+        include_once("seeders.php");
+        // Aggiungi altre classi di migrazione secondo necessità
+
+        // Lista delle classi di migrazione
+        $migrazioni = [
+            new Utente(),
+        ];
+
+        foreach ($migrazioni as $migrazione) {
+            
+            if ($migrazione) {
+                if ($migrazione->up($conn)) {
+                    
+                    echo "Migrazione completata con successo: " . get_class($migrazione) . "\n";
+                } else {
+                    echo "Errore nella migrazione: " . $conn->error . "\n";
+                }
+            } else {
+                echo "La migrazione è già stata eseguita: " . get_class($migrazione) . "\n";
+            }
+        }
+
+        $seeders = [
+            new UtenteS(),
+        ];
+
+        foreach ($seeders as $seeder) {
+            
+            if ($seeder) {
+                if ($seeder->upS($conn)) {
+                    
+                    echo "seeder completato con successo: " . get_class($seeder) . "\n";
+                } else {
+                    echo "Errore nel seeder: " . $conn->error . "\n";
+                }
+            } else {
+                echo "Il seeder è già stata eseguita: " . get_class($seeder) . "\n";
+            }
+        }
+
+        $conn->close();
+    ?>
