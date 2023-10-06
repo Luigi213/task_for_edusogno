51ed201e63476bf57626d049ba2c4d32aacd9e8b
Luigi213 <reyesgianluigi88@gmail.com>
2023-10-06 14:52:49 +0200

    finished all style

--- admin/admin.php
+++ admin/admin.php
@@ -29,80 +29,89 @@ if (!in_array('ADMIN', $_SESSION['ruoli'])) {
         <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">        
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css' integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer' />
+        <link rel="stylesheet" href="../styles/style.css">
         <title>Document</title>
     </head>
     <body>
-        <header class="py-4 d-flex my-bg-primary flex-md-nowrap align-items-center px-4 shadow my-header justify-content-between">
-            <a href="admin.php">
-                <img src="../image/Group_181.png" alt="">
-            </a>
-            <div class="navbar nav">
-                <div class="d-flex align-items-center">
-                    <a href="../logout.php">logout</a></p>
+        <div class="container-sp">
+        <header>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-10 pad-cust">
+                        <img  class="img-edu"src="../image/Group_181.png" alt="logo">
+                    </div>
+                    <div class="col-2 d-flex align-items-center justify-content-center">
+                        <a class="logout p-2" href="logout.php">logout</a>
+                    </div>
                 </div>
             </div>
         </header>
-
-            
-        <div class="container-fluid mt-3">
-            <div class="h-100 d-flex">
-                <nav class="col-md-3 col-lg-2 fw-bold">
-                    <ul class="nav flex-column">
-                        <li class="nav-item rounded">
-                            <a class="nav-link" href="#">
-                                <i class="fa-solid fa-tachometer-alt fa-lg fa-fw"></i> Eventi
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
-                <main class="col-md-9">
-                    <div class="container">
-                        <h1>ciao admin</h1>
-                        <div class="row">
-                            <div class="col-md-9 col-lg-12">
-                                <a href='aggiungi.php'>aggiungi nuovo evento</a>
-                                <table class="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th scope="col">Nome evento</th>
-                                            <th scope="col">Data evento</th>
-                                            <th scope="col">Partecipanti</th>
-                                            <th scope="col">Azioni</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php 
-                                            foreach($eventi as $evento){
-                                                echo "<tr>";
-                                                echo "<td>" . $evento['nome_evento'] . "</td>";
-                                                echo "<td>" . $evento['data_evento'] . "</td>";
-                                                echo "<td>" . $evento['attendees'] . "</td>";
-                                                echo "<td class='d-flex justify-content-between'>
-                                                        <form action='modifica.php' method='POST'>
-                                                            <input type='hidden' name='id' value='" . $evento["id"] . "'>
-                                                            <button class='btn' type='submit'>
-                                                                <i class='fa-solid fa-pen-to-square'></i>
-                                                            </button>
-
-                                                        </form>
-                                                        <form method='POST'>
-                                                            <input type='hidden' name='action' value='delete'>
-                                                            <input type='hidden' name='id_delete' value='" . $evento["id"] . "'>
-                                                            <button class='btn' type='submit'>
-                                                                <i class='fa-solid fa-dumpster-fire'></i>
-                                                            </button>
-                                                        </form>
-                                                    </td>";
-                                                echo "</tr>";
-                                            }
-                                        ?>
-                                    </tbody>
-                                </table>
-                            </div>
+        <main>  
+            <div class="container-fluid mt-cm">
+                <div class="row justify-content-center">
+                    <div class="col-sp">
+                        <div class="mb-4">
+                            <a class="logout p-3" href='aggiungi.php'>aggiungi nuovo evento</a>
                         </div>
+                        <table class="table table-sp">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Nome evento</th>
+                                    <th scope="col">Data evento</th>
+                                    <th scope="col">Partecipanti</th>
+                                    <th scope="col">Azioni</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                    foreach($eventi as $evento){
+                                        echo "<tr>";
+                                        echo "<td>" . $evento['nome_evento'] . "</td>";
+                                        echo "<td>" . $evento['data_evento'] . "</td>";
+                                        echo "<td>" . $evento['attendees'] . "</td>";
+                                        echo "<td class='d-flex justify-content-between'>
+                                                <form action='modifica.php' method='POST'>
+                                                    <input type='hidden' name='id' value='" . $evento["id"] . "'>
+                                                    <button class='btn' type='submit'>
+                                                        <i class='fa-solid fa-pen-to-square'></i>
+                                                    </button>
+
+                                                </form>
+                                                <form method='POST'>
+                                                    <input type='hidden' name='action' value='delete'>
+                                                    <input type='hidden' name='id_delete' value='" . $evento["id"] . "'>
+                                                    <button class='btn' type='submit'>
+                                                        <i class='fa-solid fa-dumpster-fire'></i>
+                                                    </button>
+                                                </form>
+                                            </td>";
+                                        echo "</tr>";
+                                    }
+                                ?>
+                            </tbody>
+                        </table>
                     </div>
-                </main>
+                </div>
             </div>
+        </main>
+        <div class="eclipse-1">
+            <img src="../image/Ellipse_13.png" alt="ellipse-13">
         </div>
+        <div class="eclipse-2">
+            <img src="../image/Ellipse_12.png" alt="ellipse-12">
+        </div>
+        <div class="vector-1">
+            <img id="vec-1" src="../image/Vector_1.png" alt="vector-1">
+        </div>
+        <div class="vector-4">
+            <img id="vec-4" src="../image/Vector_4.png" alt="vector-4">
+        </div>
+        <div class="vector-5">
+            <img id="vec-5" src="../image/Vector_5.png" alt="vector-5">
+        </div>
+        <div class="group-201">
+            <img id="group-201" src="../image/Group_201.png" alt="group-201">
+        </div>
+    </div>
     </body>
 </html>
